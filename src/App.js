10195a6db69e75a0d5480e5d61c7c5278421ff2e
Adam Wojnicki <wojnicki.ada1@gmial.com>
2020-10-06 17:58:45 +0200

    Initial markup setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function App() {
-  return <div>DUPA</div>;
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>#todo</h1>
+        </header>
+        <main>
+          <div className="filters">
+            <button className="filters__btn active">All</button>
+            <button className="filters__btn">Active</button>
+            <button className="filters__btn">Completed</button>
+          </div>
+          <form>
+            <input type="text" />
+            <input type="submit" value="Add" />
+          </form>
+          <ul className="todo-list">
+            <li className="todo-list__item">Todo 1</li>
+            <li className="todo-list__item">Todo 2</li>
+            <li className="todo-list__item">Todo 3</li>
+          </ul>
+        </main>
+        <footer>Adam Wojnicki @ DevChallenges.io</footer>
+      </div>
+    );
+  }
 }
