e5e1a170b669a384dae3ff0fd1c053e4846550b4
Adam Wojnicki <wojnicki.ada1@gmial.com>
2020-10-12 22:36:12 +0200

    Added delete icon and improved UX

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,13 +3,19 @@ import React from "react";
 export default ({ todo, onCheck }) => {
   return (
     <li className={`todo-list__item ${todo.completed ? "completed" : ""}`}>
-      <input
-        className="todo-list__checkbox"
-        type="checkbox"
-        checked={todo.completed}
-        onChange={onCheck}
-      />
-      {todo.text}
+      <label>
+        <input
+          className="todo-list__checkbox"
+          type="checkbox"
+          checked={todo.completed}
+          onChange={onCheck}
+        />
+        {todo.text}
+      </label>
+      {todo.completed ? (<button>
+        <i className="material-icons">delete_outline</i>
+      </button>) : null }
+      
     </li>
   );
 };
